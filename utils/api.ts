a8174a89f84b0f92c559ab4dc648fc805599817f
sam <s.ojling@gmail.com>
2021-03-19 14:59:46 -0700

    Added metric unit as a param to the api call

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 async function fetchWeather(location) {
   try {
     const result = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a17480f70f0d4368ad0b5eabd0e37b66`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=a17480f70f0d4368ad0b5eabd0e37b66`,
     );
     const data = await result.json();
 
