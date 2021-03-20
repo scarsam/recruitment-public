fe12d45aeba33ca65ee1f33b495df88fb750a070
sam <s.ojling@gmail.com>
2021-03-19 17:01:13 -0700

    Added label to support targeting the form inside my test

--- components/Form/WeatherForm.tsx
+++ components/Form/WeatherForm.tsx
@@ -12,13 +12,14 @@ const WeatherForm = ({ handleSubmit }) => {
 
   return (
     <form className={styles.form} onSubmit={onSubmit}>
+      <label htmlFor="city">Location</label>
       <input
+        id="city"
         type="text"
         placeholder="Enter City"
         value={location}
         onChange={(e) => setLocation(e.target.value)}
       />
-
       <button type="submit">Get Weather</button>
     </form>
   );
